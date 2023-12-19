8a89400db5d68cd61025b87ae41ab55f56aa96bd
Marietje86 <81444491+Marietje86@users.noreply.github.com>
2023-12-19 18:45:47 +0100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generateRecipe(event) {
 
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "165fb30cc9162832cte4113b00adoc29";
-let context = "You are a great healthy Chef expert and love to write quick, easy recipes. Your mission is to generate a 25 line recipe in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions. Sign the recipe with 'Enjoy your meal!' inside a <strong> element at the end of the recipe";
+let context = "You are a great healthy Chef expert and love to write quick, easy recipes. Your mission is to generate a 25 line recipe in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions. Sign the recipe with 'Enjoy your meal!' inside a <strong> element at the end of the recipe as a <br /> line. Include the sign in the recipe. Include the list of ingredients above at the top of the instructions in basic HTML and seperate each line with a <br /.";
 let prompt = `User Instructions: Generate a healthy food recipe of ${instructionsInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let recipeElement = document.querySelector("#recipe");
+recipeElement.classList.remove("hidden");
+recipeElement.innerHTML = `<div class="generating">⏳Generating a Healthy food recipe with ${instructionsInput.value}</div>`;
 
 console.log("generating recipe");
 console.log(`Prompt: ${prompt}`);
